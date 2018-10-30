a87dbb547c5f9546021432f74ba211c084c1d978
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2018-10-30 14:18:16 -0400

    init update
    Updated init file to:
    1) include module information
    2) follow PEP8 style
    3) allow normal module import of ssj_auroral_boundary

--- ssj_auroral_boundary/__init__.py
+++ ssj_auroral_boundary/__init__.py
@@ -4,8 +4,34 @@
 # Space Environment Data Analysis Group (SEDA)
 # Colorado Center for Astrodynamics Research (CCAR)
 # University of Colorado, Boulder (CU Boulder)
-__all__ = ['absatday','abpolarpass','absegment','abcsv',
-												'files','dmsp_spectrogram']
-												
+"""
+ssj_auroral_boundary
+--------------------
+
+Figure of Merit boundary identification for DMSP SSJ5
+
+Modules
+-------
+absatday
+abpolarpass
+absegment
+abscv
+files
+dmsp_spectrogram
+
+"""
+from __future__ import print_function
+
+__version__ = "0.1"
+
 #Prefix for all package loggers
 loggername = 'ssj_auroral_boundary'
+
+# Import all the modules
+__all__ = ['absatday', 'abpolarpass', 'absegment', 'abcsv', 'files',
+           'dmsp_spectrogram']
+
+try:
+    from ssj_auroral_boundary import __all__
+except ImportError as err:
+    print("problem importing {:s}: {:}".format(loggername, err))
