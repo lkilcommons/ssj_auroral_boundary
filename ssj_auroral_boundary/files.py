d4345c5b1e8d66943220e24e23b7cb463314c27a
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2018-10-30 17:21:00 -0400

    PEP8 tabs
    Changed irregularly sized tabs to standard 4 spaces

--- ssj_auroral_boundary/files.py
+++ ssj_auroral_boundary/files.py
@@ -15,84 +15,84 @@ download_cdf_from_noaa : download remote file using url output
 """
 
 def test_cdf_path_and_filename():
-        """ Find location of test file
+    """ Find location of test file
 
-        Returns
-        -------
-        test_data_dir : string
-            Test data directory
-        test_cdffn : string
-            Test data filename
+    Returns
+    -------
+    test_data_dir : string
+        Test data directory
+    test_cdffn : string
+        Test data filename
 
-        """
-        import os
+    """
+    import os
 
-	#Determine where this module's source file is located
-	#to determine where to look for the test data
-	src_file_dir = os.path.dirname(os.path.realpath(__file__))
-	test_data_dir = os.path.join(src_file_dir, 'test_data')
-	test_cdffn = 'dmsp-f16_ssj_precipitating-electrons-ions_20100529_v1.1.2.cdf'
-	return test_data_dir, test_cdffn
+    #Determine where this module's source file is located
+    #to determine where to look for the test data
+    src_file_dir = os.path.dirname(os.path.realpath(__file__))
+    test_data_dir = os.path.join(src_file_dir, 'test_data')
+    test_cdffn = 'dmsp-f16_ssj_precipitating-electrons-ions_20100529_v1.1.2.cdf'
+    return test_data_dir, test_cdffn
 
 def cdf_url_and_filename(dmsp_number, year, month, day):
-        """ Download a DMSP file from NOAA
+    """ Download a DMSP file from NOAA
 
-        Parameters
-        ----------
-        dmsp_number : int
-            DMSP satellite number (e.g. 15)
-        year : int
-            Data year
-        month : int
-            Data month (1-12)
-        day : int
-            Data day of month
+    Parameters
+    ----------
+    dmsp_number : int
+        DMSP satellite number (e.g. 15)
+    year : int
+        Data year
+    month : int
+        Data month (1-12)
+    day : int
+        Data day of month
 
-        Returns
-        -------
-        cdf_url : string
-            URL to retrieve file at
-        cdffn : string
-            CDF filename
+    Returns
+    -------
+    cdf_url : string
+        URL to retrieve file at
+    cdffn : string
+        CDF filename
 
-        """
+    """
 
-	cdffn = ('dmsp-f%.2d' % (dmsp_number)
-	 		+'_ssj_precipitating-electrons-ions_'
-			+'%d%.2d%.2d_v1.1.2.cdf' % (year,month,day))
+    cdffn = ('dmsp-f%.2d' % (dmsp_number)
+             +'_ssj_precipitating-electrons-ions_'
+            +'%d%.2d%.2d_v1.1.2.cdf' % (year,month,day))
 
-	root_url = 'https://satdat.ngdc.noaa.gov/dmsp/data/'
-	one_month_ssj_data_url = 'f%.2d/ssj/%d/%.2d/' % (dmsp_number, year,
-                                                         month)
-	#Expected CDF file
-	cdf_url = root_url + one_month_ssj_data_url + cdffn
+    root_url = 'https://satdat.ngdc.noaa.gov/dmsp/data/'
+    one_month_ssj_data_url = 'f%.2d/ssj/%d/%.2d/' % (dmsp_number, year,
+                             month)
+    #Expected CDF file
+    cdf_url = root_url + one_month_ssj_data_url + cdffn
 
-	return cdf_url, cdffn
+    return cdf_url, cdffn
 
 def download_cdf_from_noaa(cdf_url, destination_cdffn):
-        """ Download CDF from NOAA
+    """ Download CDF from NOAA
 
-        Parameters
-        ----------
-        cdf_url : string
-            URL (see cdf_url_and_filename) for remote file
-        destination_cdffn : string
-            Local name with directory destination for downloaded file
+    Parameters
+    ----------
+    cdf_url : string
+        URL (see cdf_url_and_filename) for remote file
+    destination_cdffn : string
+        Local name with directory destination for downloaded file
 
-        Returns
-        -------
-        Void
+    Returns
+    -------
+    Void
 
-        """
-        import requests
+    """
+    import requests
 
-	head = requests.head(cdf_url,allow_redirects=True)
-	headers = head.headers
-	content_type = headers.get('content-type')
-	print(headers,content_type)
-	if 'html' not in content_type.lower():
-		response = requests.get(cdf_url,allow_redirects=True)
-		with open(destination_cdffn,'w') as f:
-			f.write(response.content)
-	else:
-		raise RuntimeError('URL %s is not downloadable' % (cdf_url))
+    head = requests.head(cdf_url,allow_redirects=True)
+    headers = head.headers
+    content_type = headers.get('content-type')
+    print(headers,content_type)
+    if 'html' not in content_type.lower():
+        response = requests.get(cdf_url,allow_redirects=True)
+        with open(destination_cdffn,'w') as f:
+            f.write(response.content)
+    else:
+        raise RuntimeError('URL %s is not downloadable' % (cdf_url))
